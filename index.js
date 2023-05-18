c652fd119d1e475e2562980c6aa90799d1570bbf
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-05-18 19:12:04 +0600

    api added for the subcategory tabs of front end

--- index.js
+++ index.js
@@ -64,6 +64,16 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/toyCollectionBySubCategory/:text', async(req, res) => {
+            const searchedText = req.params.text;
+            
+            const result = await toyCollection.find({
+                subCategory: { $regex: searchedText, $options: "i" }
+            }).toArray();
+
+            res.send(result);
+        });
+
         app.get('/singleToyDetail/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
